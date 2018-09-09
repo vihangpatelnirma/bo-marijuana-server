e895bd5f0877e88546cd944da1495cc9b1d5bfb9
Vihang Patel <vihang.engg@gmail.com>
2018-09-09 18:06:15 +0530

    Add credentials for user 1

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 const db = require("./db")
 const mailer = require("./mailer")
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const speakeasy = require("speakeasy")
+const creds = require("./userCreds")
 
 const PORT = process.env.PORT || 8888
 
@@ -19,6 +22,9 @@ app.use(function(req, res, next) {
 // Configure body parser
 app.use(bodyParser.json())
 
+// Configure cookie parser
+app.use(cookieParser())
+
 // connect mongo client
 db.connectMongo()
 
@@ -26,6 +32,63 @@ db.connectMongo()
 app.post("/save-in-db", function(req, res) {
 	db.saveInDb(req, res)
 	mailer.sendEmail(req, res)
+})
+
+const verifyToken = (secret, token) =>
+	speakeasy.totp.verify({
+		secret: secret.base32,
+		encoding: "base32",
+		token: token,
+	})
+
+const verifyCookie = userCookie => +userCookie % 23 === 0
+
+const generateRandomNumber = () => {
+	var randomNumber = Math.random().toString()
+	return randomNumber.substring(5, randomNumber.length) * 23
+}
+
+app.post("/session/login", (req, res) => {
+	const token = req.body.token
+	const userName = req.body.userName
+
+	// If user name is not found return
+	if (!creds[userName]) {
+		res.send({
+			success: false,
+			message: "Invalid Attempt",
+		})
+		return
+	}
+
+	// Verify token for that perticular user
+	const verified = verifyToken(creds[userName], token)
+
+	// set cookie in response
+	res.cookie("USER", generateRandomNumber(), { maxAge: 1000 * 60 * 60 })
+
+	res.send({
+		success: Boolean(verified),
+		message: !verified ? "Invalid credentials" : "You are logged in",
+	})
+})
+
+app.post("/session/validate", (req, res) => {
+	const sessionCookie = req.cookies.USER
+
+	const isLoggedIn = verifyCookie(sessionCookie)
+	if (isLoggedIn) {
+		res.cookie("USER", generateRandomNumber(), { maxAge: 1000 * 60 * 60 })
+		res.send({
+			success: true,
+		})
+	} else {
+		res.cookie("USER", "", { maxAge: 1 })
+		res.send({
+			success: false,
+			message: "You are not logged in.",
+		})
+	}
 })
 
 app.listen(PORT, function() {
